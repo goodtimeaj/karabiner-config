b8b31dfb3381804178c33301bdc93d19b6bd12fd
gregstallings <gregstallings@gmail.com>
2013-10-13 20:10:52 -0700

    Save PCKeyboardHack settings

--- export.sh
+++ export.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 #
 # Exports KeyRemap4MacBook settings.
 
 HERE=$(dirname "$0") && HERE=$(cd "$HERE" && pwd -P)
 
 # Export general settings (KeyRemap4MacBook must be open)
-sh "$HERE/settings/kr4mb-import.sh"
+sh "$HERE/keyremap4macbook/kr4mb-import.sh"
 
 # Link KeyRemap4MacBook private.xml settings
-ln -sfv "$HERE/settings/private.xml" "$HOME/Library/Application Support/KeyRemap4MacBook"
+ln -sfv "$HERE/keyremap4macbook/private.xml" "$HOME/Library/Application Support/KeyRemap4MacBook"
+
+# Copy PCKeyboardHack preferences
+cp "$HERE/pckeyboardhack/org.pqrs.PCKeyboardHack.plist" "$HOME/Library/Preferences/org.pqrs.PCKeyboardHack.plist"
--- save.sh
+++ save.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 #
 # Saves KeyRemap4MacBook settings to this repository.
 
 HERE=$(dirname "$0") && HERE=$(cd "$HERE" && pwd -P)
 
 # Save KeyRemap4MacBook preferences
-/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli export > "$HERE/settings/kr4mb-import.sh"
+/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli export > "$HERE/keyremap4macbook/kr4mb-import.sh"
+
+# Save PCKeyboardHack preferences
+cp "$HOME/Library/Preferences/org.pqrs.PCKeyboardHack.plist" "$HERE/pckeyboardhack/org.pqrs.PCKeyboardHack.plist"
